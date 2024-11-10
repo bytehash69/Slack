f040b4f1f83ab29fbff8ec64231774cd74aa6895
S.Adithya Vardhan <168300867+Adiflickzzz@users.noreply.github.com>
2024-11-10 22:29:56 +0530

    Add workspace header

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,6 +1,12 @@
 "use client";
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { WorkspaceSidebar } from "./workspace-sidebar";
 
 const WorkspaceLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -8,7 +14,21 @@ const WorkspaceLayout = ({ children }: { children: React.ReactNode }) => {
       <Toolbar />
       <div className="flex h-[calc(100vh-40px)]">
         <Sidebar />
-        {children}</div>
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId="adi-workspace-layout"
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5E2C5F]"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
     </div>
   );
 };
---
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -0,0 +1,42 @@
+import { useCurrentMember } from "@/features/members/api/use-current-member";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { AlertTriangle, Loader2 } from "lucide-react";
+import React from "react";
+import { WorkspaceHeader } from "./workspace-header";
+
+export const WorkspaceSidebar = () => {
+  const workspaceId = useWorkspaceId();
+
+  const { data: member, isLoading: memberLoading } = useCurrentMember({
+    workspaceId,
+  });
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
+    id: workspaceId,
+  });
+
+  if (workspaceLoading || memberLoading) {
+    return (
+      <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
+        <Loader2 className="size-5 animate-spin text-white" />
+      </div>
+    );
+  }
+
+  if (!member || !workspace) {
+    return (
+      <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
+        <AlertTriangle className="size-5 text-white" />
+        <p className="text-white text-sm">Workspace not found!</p>
+      </div>
+    );
+  }
+  return (
+    <div className="flex flex-col bg-[#5E2C5F] h-full">
+      <WorkspaceHeader
+        workspace={workspace}
+        isAdmin={member.role === "admin"}
+      />
+    </div>
+  );
+};
